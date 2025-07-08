8db66adb298b3fd6de371aaf03a11b7c7de447af
Nick Ramirez <nick.ramirez@arborxr.com>
2025-07-07 17:26:12 -0700

    Lock Y-axis for UI prompts

--- Runtime/UI/FaceCamera.cs
+++ Runtime/UI/FaceCamera.cs
@@ -8,13 +8,26 @@ public class FaceCamera : MonoBehaviour
     [Tooltip("Vertical offset from the camera's eye height (in meters)")]
     public float verticalOffset = 0;
 
-    private void LateUpdate()
+    private void Start()
     {
         var cam = Camera.main.transform;
         if (!cam) return;
         
         Vector3 targetPos = cam.position + cam.forward * distanceFromCamera + Vector3.up * verticalOffset;
         transform.position = targetPos;
+
+        // face the camera
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.position, Vector3.up);
+    }
+
+    private void LateUpdate()
+    {
+        var cam = Camera.main.transform;
+        if (!cam) return;
+        
+        Vector3 targetPos = cam.position + cam.forward * distanceFromCamera + Vector3.up * verticalOffset;
+        Vector3 newPos = new Vector3(targetPos.x, transform.position.y, targetPos.z);
+        transform.position = newPos;
 
         // face the camera
         transform.rotation = Quaternion.LookRotation(transform.position - cam.position, Vector3.up);
