6aebf1923a144bd896745c95d742d8137b91b2e4
Nick Ramirez <nick.ramirez@arborxr.com>
2025-08-15 10:52:18 -0700

    Lock UI screen to a constant Y

--- Runtime/UI/FaceCamera.cs
+++ Runtime/UI/FaceCamera.cs
@@ -8,25 +8,13 @@ public class FaceCamera : MonoBehaviour
     [Tooltip("Vertical offset from the camera's eye height (in meters)")]
     public float verticalOffset = 0;
 
-    private void Start()
-    {
-        var cam = Camera.main.transform;
-        if (!cam) return;
-        
-        Vector3 targetPos = cam.position + cam.forward * distanceFromCamera + Vector3.up * verticalOffset;
-        transform.position = targetPos;
-
-        // face the camera
-        transform.rotation = Quaternion.LookRotation(transform.position - cam.position, Vector3.up);
-    }
-
     private void LateUpdate()
     {
         var cam = Camera.main.transform;
         if (!cam) return;
         
-        Vector3 targetPos = cam.position + cam.forward * distanceFromCamera + Vector3.up * verticalOffset;
-        Vector3 newPos = new Vector3(targetPos.x, transform.position.y, targetPos.z);
+        Vector3 targetPos = cam.position + cam.forward * distanceFromCamera;
+        Vector3 newPos = new Vector3(targetPos.x, 1.1f + verticalOffset, targetPos.z);
         transform.position = newPos;
 
         // face the camera
