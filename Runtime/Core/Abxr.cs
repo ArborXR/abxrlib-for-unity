9f8ae142719e168f708861abd8214fb305f46bce
Dan Kuykendall <dan@kuykendall.org>
2025-08-22 14:05:10 -0700

    testing still

--- Runtime/Core/Abxr.cs
+++ Runtime/Core/Abxr.cs
@@ -422,9 +422,9 @@ namespace Abxr.Runtime.Core
 		/// <param name="pollType">What kind of poll would you like</param>
 		/// <param name="responses">If a multiple choice poll, you need to provide between 2 and 8 possible responses</param>
 		/// <param name="callback">Optional callback that will be called with the selected string value (Multiple-choice poll only)</param>
-		public static void PollUser(string prompt, ExitPollHandler.PollType pollType, List<string> responses = null, Action<string> callback = null)
+		public static void PollUser(string prompt, Abxr.Runtime.UI.ExitPoll.ExitPollHandler.PollType pollType, List<string> responses = null, Action<string> callback = null)
 		{
-			if (pollType == ExitPollHandler.PollType.MultipleChoice)
+			if (pollType == Abxr.Runtime.UI.ExitPoll.ExitPollHandler.PollType.MultipleChoice)
 			{
 				if (responses == null)
 				{
