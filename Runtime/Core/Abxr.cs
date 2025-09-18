87b5dbde07c9e6b6e6d1869f7ed3b40238073630
Dan Kuykendall <dan@kuykendall.org>
2025-09-18 09:43:51 -0700

    Removed defunct line

--- Runtime/Core/Abxr.cs
+++ Runtime/Core/Abxr.cs
@@ -1089,8 +1089,7 @@ public static partial class Abxr
 		return new CurrentSessionData(
 			currentModule.target,
 			GetUserData(),
-			Authentication.GetAuthResponse().UserId,
-			Authentication.GetAuthHandoffData().userEmail
+			Authentication.GetAuthResponse().UserId
 		);
 	}
 
