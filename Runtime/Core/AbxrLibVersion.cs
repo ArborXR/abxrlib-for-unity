6e985267eec4773c9de48ae6d713311c554c530f
Nick Ramirez <nick.ramirez@arborxr.com>
2025-12-16 12:34:54 -0800

    Update version

--- Runtime/Core/AbxrLibVersion.cs
+++ Runtime/Core/AbxrLibVersion.cs
@@ -2,6 +2,6 @@ namespace AbxrLib.Runtime.Core
 {
     public static class AbxrLibVersion
     {
-        public const string Version = "1.3.0";
+        public const string Version = "1.3.1";
     }
 }
