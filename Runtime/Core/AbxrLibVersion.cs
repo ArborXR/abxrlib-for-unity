630cfdcaa3eadc7b5209ccd99dc050f3aaa252b2
Dan Kuykendall <dan@kuykendall.org>
2026-03-31 12:02:08 -0700

    version bump

--- Runtime/Core/AbxrLibVersion.cs
+++ Runtime/Core/AbxrLibVersion.cs
@@ -2,6 +2,6 @@ namespace AbxrLib.Runtime.Core
 {
     public static class AbxrLibVersion
     {
-        public const string Version = "2.0.9";
+        public const string Version = "2.0.10";
     }
 }
